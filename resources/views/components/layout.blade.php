b7101377c51eab8475a081481faf8bd809246118
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-07-12 17:48:57 +0530

    added @if directive around footer
    if create url then hide footer.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -42,13 +42,14 @@
         {{ $slot }}
     </main>
 
-    <footer
-        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-20 mt-24 opacity-90 md:justify-center">
-        <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+    @if (!(Request::url() === 'http://127.0.0.1:8000/listings/create'))
+        <footer
+            class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-20 mt-24 opacity-90 md:justify-center">
+            <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
 
-        <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
-    </footer>
-
+            <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
+        </footer>
+    @endif
 
 </body>
 
