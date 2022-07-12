8793e1bfc1b58262b15455bb65f7b6ae79895b62
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-07-12 09:19:02 +0530

    all files get migrated now
    + created eloquent model Listing using command

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,25 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing{
-    public static function all(){
-        return   [
-            [  'id' => 1,
-            'title' => 'Listing One',
-            'description' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi nam, enim ut sit aliquid beatae dolores magni, ex fugit aspernatur ea eos nisi porro voluptatem iure magnam et vitae vel!'],
-            ['id' => 2,
-            'title' => 'Listing Two',
-            'description' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi nam, enim ut sit aliquid beatae dolores magni, ex fugit aspernatur ea eos nisi porro voluptatem iure magnam et vitae vel!']
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
- public static function find($id){
-$listings = self::all();
-
-foreach($listings as $listing){
-    if($listing['id'] == $id){
-        return $listing;
-    }
-   }   
-  }
-} 
+class Listing extends Model
+{
+    use HasFactory;
+}
