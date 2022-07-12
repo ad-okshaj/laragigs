ce3f7090db240aca417f478d9aa103300d96ab99
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-07-12 23:41:39 +0530

    minor styling changes.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,7 +26,7 @@
 </head>
 
 <body class="mb-48">
-    <nav class="flex justify-between items-center mb-4">
+    <nav class="flex justify-between items-center mb-0.5">
         <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt=""
                 class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
